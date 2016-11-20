9607a75b4b7df5dcd5c1c4a9a27b317b57d1aa61
Grace Zhang <gz.gracez16@gmail.com>
2016-11-20 12:46:39 -0500

    test sql

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from cs50 import SQL
+from sql import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from passlib.apps import custom_app_context as pwd_context
