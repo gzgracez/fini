9a742027712e539f042ff5a2b6034ae0f7e47223
Grace Zhang <gz.gracez16@gmail.com>
2016-12-02 01:40:50 -0500

    db, center

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///fini.db")
 
 @app.route("/")
 @login_required
