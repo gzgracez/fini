93de96b79e0dae16275687da3096916e88e0c393
Sander Coates <Sander@Senna.local>
2016-12-01 23:08:46 -0500

    ... git commit -m ...

--- helpers.py
+++ helpers.py
@@ -82,25 +82,25 @@ def transactions_display(shares, cash):
         i["name"] = info["name"]
     return [shares, usd(total)]
 
-def lookup(geo):
-    """Looks up articles for geo."""
+# def lookup(geo):
+#     """Looks up articles for geo."""
 
-    # check cache for geo
-    if geo in lookup.cache:
-        return lookup.cache[geo]
+#     # check cache for geo
+#     if geo in lookup.cache:
+#         return lookup.cache[geo]
 
-    # get feed from Google
-    feed = feedparser.parse("http://news.google.com/news?geo={}&output=rss".format(urllib.parse.quote(geo, safe="")))
+#     # get feed from Google
+#     feed = feedparser.parse("http://news.google.com/news?geo={}&output=rss".format(urllib.parse.quote(geo, safe="")))
 
-    # if no items in feed, get feed from Onion
-    if not feed["items"]:
-        feed = feedparser.parse("http://www.theonion.com/feeds/rss")
+#     # if no items in feed, get feed from Onion
+#     if not feed["items"]:
+#         feed = feedparser.parse("http://www.theonion.com/feeds/rss")
 
-    # cache results
-    lookup.cache[geo] = [{"link": item["link"], "title": item["title"]} for item in feed["items"]]
+#     # cache results
+#     lookup.cache[geo] = [{"link": item["link"], "title": item["title"]} for item in feed["items"]]
 
-    # return results
-    return lookup.cache[geo]
+#     # return results
+#     return lookup.cache[geo]
 
-# initialize cache
-lookup.cache = {}
+# # initialize cache
+# lookup.cache = {}
